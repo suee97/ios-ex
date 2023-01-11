2218ced6233f673870d2e6dfb164fe0924cc4123
suee97 <atn1su@gmail.com>
2023-01-12 02:23:58 +0900

    init

---
+++ exp/LabelView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@IBDesignable
+class LabelView: UILabel {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    
+}
